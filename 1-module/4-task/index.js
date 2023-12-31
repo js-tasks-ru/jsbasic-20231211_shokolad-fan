289cab18767bc473f26dc7740cd9856c00f02d90
Armazok <nikolai.borsin@mail.ru>
2023-12-31 21:06:57 +0300

    Completed 5 assignments in 1 module with comments and additional checks

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,6 +1,32 @@
 function checkSpam(str = '') {
-  //  код
+  if (typeof str !== 'string' || !str) {
+    throw new Error('Передан не строковый аргумент');
+  }
+
+
+  // Создаем хэш таблицу
+  let hashTable = ['1xBet', 'XXX'];
+  // Создаем новый массив и приводим его весь к нижнему регистру
+  let lowerCaseHashTableTable = hashTable.map(el => el.toLowerCase());
+
+
+  // Проводим итерации через цикл for of
+  for (let word of lowerCaseHashTableTable) {
+    // Выполняем условие
+    // 1. Приводим аргумент (параметр) str к нижнему регистру
+    // 2. Ищем совпадения в хэш таблице
+    // Вместо побитового оператора можно через -1 и includes
+    if (~(str.toLowerCase().indexOf(word))) {
+      return true;
+    }
+  }
+
+  return false;
 }
 
 
-console.log('checkSpam: ', checkSpam('1Xbet') === true);
+
+// let counter1 = 1;
+// let result = counter1++;
+// console.log('result: ', result); // result = 1,
+// console.log('counter1: ', counter1); // counter = 2
