0ad619b6513cd02faa428cc6eabd5b7d941373e2
Armazok <nikolai.borsin@mail.ru>
2024-01-15 12:14:35 +0300

    4 Модуль 3 задачи

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,23 @@
-function makeFriendsList(friends) {
-  // ваш код...
+function makeFriendsList(friends = []) {
+
+  // Создаем через document тег 'ul'
+  let ulTag = document.createElement('ul');
+
+  // Итерируемся по массиву friends
+  for (let friend of friends) {
+
+    // Создаем через document тег 'li'
+    let liTag = document.createElement('li');
+
+    // Добавляем текстовое содержимое в элемент <li>
+    liTag.textContent = `${friend.firstName} ${friend.lastName}`;
+
+    // С помощью appendChild добавляет новый узел в конец списка дочерних элементов
+    ulTag.appendChild(liTag);
+  }
+
+  // Возвращаем тег ul и все его дочерние элементы
+  return ulTag;
 }
+
+
--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,35 @@
 function highlight(table) {
-  // ваш код...
+  let rows = table.getElementsByTagName('tbody')[0].getElementsByTagName('tr');
+
+  for (let row of rows) {
+    let statusCell = row.getElementsByTagName('td')[3];
+    let genderCell = row.getElementsByTagName('td')[2];
+    let ageCell = row.getElementsByTagName('td')[1];
+
+    // Установка класса available/unavailable в зависимости от значения атрибута data-available
+    if (statusCell.hasAttribute('data-available')) {
+      let available = statusCell.getAttribute('data-available');
+      if (available === 'true') {
+        statusCell.parentNode.classList.add('available');
+      } else {
+        statusCell.parentNode.classList.add('unavailable');
+      }
+    } else {
+      // Установка атрибута hidden, если атрибута data-available нет вообще
+      statusCell.parentNode.setAttribute('hidden', '');
+    }
+
+    // Установка класса male/female в зависимости от значения ячейки Gender
+    if (genderCell.textContent === 'm') {
+      genderCell.parentNode.classList.add('male');
+    } else if (genderCell.textContent === 'f') {
+      genderCell.parentNode.classList.add('female');
+    }
+
+    // Добавление inline-стиля text-decoration: line-through, если значение ячейки Age меньше 18
+    let ageValue = parseInt(ageCell.textContent);
+    if (ageValue < 18) {
+      ageCell.parentNode.style.textDecoration = 'line-through';
+    }
+  }
 }
