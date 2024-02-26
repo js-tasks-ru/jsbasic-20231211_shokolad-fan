99feff3c2e4c58696cc39e29ffb60ae4e1be6364
Armazok <nikolai.borsin@mail.ru>
2024-02-26 09:43:55 +0300

    Рефактор 5 модуля

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,58 +1,34 @@
 function initCarousel() {
-  // buttons
-  let carouselArrowLeft = document.querySelector('.carousel__arrow_left');
-  let carouselArrowRight = document.querySelector('.carousel__arrow_right');
-
-  // carouseArrowAll
-  let carouseArrowAll = document.querySelector('.carousel');
-
-  // carousel
-  let carousel = document.querySelector('.carousel__inner');
-  let carouselSlide = document.querySelectorAll('.carousel__slide');
-
-  // counter
+  let carousel = document.querySelector('.carousel');
+  let carouselLength = document.querySelectorAll('.carousel__slide').length;
   let counter = 0;
 
-  // carouselWidth
-  let carouselWidth = carousel.offsetWidth;
 
-  updateButtonVisibility();
+  updateCarouselButtons();
+  carousel.addEventListener('click', function (e) {
+    let arrowRight = e.target.closest('.carousel__arrow_right');
+    let arrowLeft = e.target.closest('.carousel__arrow_left');
+    let carouselInner = carousel.querySelector('.carousel__inner');
+    let carouselInnerWidth = carouselInner.offsetWidth;
 
-  carouseArrowAll.addEventListener('click', {
-    handleEvent(e) {
-      let target = e.target.closest('DIV');
-
-      if (target) {
-
-        if (target.classList.contains('carousel__arrow_left')) {
-          counter--;
-          carousel.style.transform = 'translateX(' + `${-carouselWidth * counter}px)`;
-
-        } else if (target.classList.contains('carousel__arrow_right')) {
-          counter++;
-          carousel.style.transform = 'translateX(' + `${-carouselWidth * counter}px)`;
-        }
-
-        updateButtonVisibility();
-      }
+    if (arrowRight || arrowLeft) {
+      // Если arrowRight равен true (нажали, 1 == true), то counter увеличится на 1, в противном случае уменьшится на -1
+      counter += arrowRight ? 1 : -1;
+      carouselInner.style.transform = `translateX(${-carouselInnerWidth * counter}px)`;
+      updateCarouselButtons();
     }
-  },);
+  }.bind(this));
 
+  function updateCarouselButtons() {
+    let carouselLeft = document.querySelector('.carousel__arrow_left');
+    let carouselRight = document.querySelector('.carousel__arrow_right');
 
-  function updateButtonVisibility() {
-    // Показываем или скрываем кнопку влево в зависимости от положения слайда
-    if (counter <= 0) {
-      carouselArrowLeft.style.display = 'none';
-    } else {
-      carouselArrowLeft.style.display = '';
-    }
+    counter === 0
+      ? carouselLeft.style.display = 'none'
+      : carouselLeft.style.display = '';
 
-    // Показываем или скрываем кнопку вправо в зависимости от положения слайда
-    if (counter >= carouselSlide.length - 1) {
-      carouselArrowRight.style.display = 'none';
-    } else {
-      carouselArrowRight.style.display = '';
-    }
+    counter === carouselLength - 1
+      ? carouselRight.style.display = 'none'
+      : carouselRight.style.display = '';
   }
-
 }
